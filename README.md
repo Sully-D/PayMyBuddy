2f6bae2259dedd6d9d2ac692d034e9b0be818fab
sully <delemos.sullivan@gmail.com>
2024-04-08 16:03:03 +0200

    Basic configuration

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PayMyBuddy
 Financial management application with payment between friends
+
+# Database schema
+![SchemaBDD.drawio.png](SchemaBDD.drawio.png)
+
+# Diagram UML
+![UML.drawio.png](UML.drawio.png)
---
+++ src/main/resources/static/sqlCommands.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE IF NOT EXISTS db_payMyBuddy;
+USE db_payMyBuddy;
+
+CREATE TABLE User (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    firstName VARCHAR(50) NOT NULL,
+    balance DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE SenderRecipientConnection (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    idSender INT NOT NULL,
+    idRecipient INT NOT NULL,
+    FOREIGN KEY (idSender) REFERENCES User(id),
+    FOREIGN KEY (idRecipient) REFERENCES User(id)
+);
+
+CREATE TABLE Transaction (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    idSender INT NOT NULL,
+    idRecipient INT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    date DATETIME NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    FOREIGN KEY (idSender) REFERENCES User(id),
+    FOREIGN KEY (idRecipient) REFERENCES User(id)
+);
