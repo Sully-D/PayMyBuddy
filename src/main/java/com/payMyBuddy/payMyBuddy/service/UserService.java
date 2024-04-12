6244970e13ab97ce51b54aa4e4572336208ca3ae
sully <delemos.sullivan@gmail.com>
2024-04-12 11:08:07 +0200

    Add JavaDocs for UserService.createUser

--- src/main/java/com/payMyBuddy/payMyBuddy/service/UserService.java
+++ src/main/java/com/payMyBuddy/payMyBuddy/service/UserService.java
@@ -24,35 +24,52 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Creates a new user account in the system with initial checks for data validity,
+     * email uniqueness, and secure password storage.
+     *
+     * @param newUser The {@link UserAccount} object containing the new user's data.
+     *                It must not be {@code null}, and all fields must be properly initialized.
+     * @throws IllegalArgumentException If any essential attribute of {@code newUser} is {@code null}.
+     * @throws UserAlreadyExistsException If a user with the same email already exists in the system.
+     */
     public void createUser(UserAccount newUser) {
 
         logger.info("Start UserService.createUser()");
 
+        // Validates non-null fields of the new user
         Utils.checkArguments(newUser.getEmail(), "Email");
         Utils.checkArguments(newUser.getPassword(), "Password");
         Utils.checkArguments(newUser.getLastName(), "LastName");
         Utils.checkArguments(newUser.getFirstName(), "FirstName");
         Utils.checkArguments(newUser.getBalance(), "Balance");
 
+        // Validates the format of the email and password
         Utils.checkEmailFormat(newUser.getEmail());
         Utils.checkPasswordFormat(newUser.getPassword());
 
+        // Checks if an existing user with the same email already exists
         Optional<UserAccount> existingUser = userRepository.findByEmail(newUser.getEmail());
         if (existingUser.isPresent()) {
             throw new UserAlreadyExistsException("A user with this email already exists : " + newUser.getEmail());
         }
 
-        // Hash password
-        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+        // Hashes the user's password for secure storage
+        String encodedPassword = bCryptPasswordEncoder.encode(newUser.getPassword());
+        newUser.setPassword(encodedPassword);
+
+        // Creates a new user account with zero initial balance
         UserAccount saveUser = UserAccount.builder()
                 .email(newUser.getEmail())
-                .password(bCryptPasswordEncoder.encode(newUser.getPassword()))
+                .password(encodedPassword)
                 .lastName(newUser.getLastName())
                 .firstName(newUser.getFirstName())
                 .balance(BigDecimal.valueOf(0.00))
                 .build();
 
         logger.info("New user saved");
+
+        // Persist the new user in the repository
         userRepository.save(saveUser);
     }
 }
