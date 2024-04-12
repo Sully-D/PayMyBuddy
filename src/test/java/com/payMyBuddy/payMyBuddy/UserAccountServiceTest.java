c401aa1ff456644490496cd2755b749fac11fbcf
sully <delemos.sullivan@gmail.com>
2024-04-12 10:14:40 +0200

    Test encrypt password

--- src/test/java/com/payMyBuddy/payMyBuddy/UserAccountServiceTest.java
+++ src/test/java/com/payMyBuddy/payMyBuddy/UserAccountServiceTest.java
@@ -10,6 +10,7 @@ import com.payMyBuddy.payMyBuddy.model.UserAccount;
 import com.payMyBuddy.payMyBuddy.repository.UserRepository;
 import com.payMyBuddy.payMyBuddy.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -22,11 +23,15 @@ import java.util.Optional;
 @SpringBootTest
 public class UserAccountServiceTest {
 
-    @Autowired
-    private UserService userService;
-
     @MockBean
     private UserRepository userRepository;
+
+    @MockBean
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @InjectMocks
+    @Autowired
+    private UserService userService;
 
     @Test
     public void createNewUser() {
@@ -113,5 +118,27 @@ public class UserAccountServiceTest {
         assertThrows(IllegalArgumentException.class, () -> userService.createUser(newUserAccount));
 
         verify(userRepository, never()).save(any(UserAccount.class));
+    }
+
+    @Test
+    public void createUser_EncryptPassword() {
+        // Given
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
+        when(bCryptPasswordEncoder.encode(anyString())).thenAnswer(invocation -> "hashed" + invocation.getArgument(0));
+
+        UserAccount newUser = UserAccount.builder()
+                .email("john.doe@test.com")
+                .password("Azerty123")
+                .lastName("Doe")
+                .firstName("John")
+                .balance(BigDecimal.valueOf(0.00))
+                .build();
+
+        // When
+        userService.createUser(newUser);
+
+        // Then
+        assertNotEquals("Azerty123", newUser.getPassword(), "Password must be encrypted");
+        assertTrue(newUser.getPassword().startsWith("hashed"), "The encrypted password must begin with 'hashed'.");
     }
 }
