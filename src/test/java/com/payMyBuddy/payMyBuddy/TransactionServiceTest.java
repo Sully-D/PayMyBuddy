c56ac9bb696d1af5549938605f7a6fa3463b8412
sully <delemos.sullivan@gmail.com>
2024-05-06 11:13:31 +0200

    Add test createTransaction_WhenFundsIsInsiffufficient()

--- src/test/java/com/payMyBuddy/payMyBuddy/TransactionServiceTest.java
+++ src/test/java/com/payMyBuddy/payMyBuddy/TransactionServiceTest.java
@@ -222,6 +222,53 @@ public class TransactionServiceTest {
     }
 
     @Test
+    public void createTransaction_WhenFundsIsInsiffufficient() {
+        // Given
+        LocalDateTime now = LocalDateTime.now();
+
+        UserAccount newUserJohn = UserAccount.builder()
+                .id(100L)
+                .email("john.doe@test.com")
+                .password("Azerty123!")
+                .lastName("Doe")
+                .firstName("John")
+                .balance(BigDecimal.valueOf(0.00))
+                .role("USER")
+                .build();
+
+        UserAccount newUserJane = UserAccount.builder()
+                .id(101L)
+                .email("jane.doe@test.com")
+                .password("Azerty123!")
+                .lastName("Doe")
+                .firstName("Jane")
+                .balance(BigDecimal.valueOf(50.00))
+                .role("USER")
+                .build();
+
+        Transaction transaction = Transaction.builder()
+                .sender(newUserJohn)
+                .recipient(newUserJane)
+                .amount(BigDecimal.valueOf(20))
+                .description("Test")
+                .date(now)
+                .build();
+
+        when(senderRecipientConnectionService.getConnection(newUserJohn)).thenReturn(Collections.singletonList("jane.doe@example.com"));
+        when(userRepository.save(any(UserAccount.class))).then(returnsFirstArg());
+        when(transactionRepository.save(any(Transaction.class))).thenReturn(transaction);
+
+        // When & Then
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            transactionService.createTransaction(transaction);
+        });
+
+        // Then
+        assertEquals("Insufficient funds", exception.getMessage());
+        verify(transactionRepository, never()).save(any(Transaction.class));
+    }
+
+    @Test
     public void getTransaction_Successfully(){
         // Given
         LocalDateTime now = LocalDateTime.now();
